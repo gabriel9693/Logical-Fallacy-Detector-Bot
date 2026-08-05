b506c8a151c12a49534bfc01a52804d25ae8669b
gabriel9693 <bobeicagabriel908@gmail.com>
2026-08-05 15:53:41 +0300

    fix #3 (btw this is a simple ver)

--- src/app/events/clientReady/log.ts
+++ src/app/events/clientReady/log.ts
@@ -1,8 +1,10 @@
 import type { EventHandler } from 'commandkit';
 import { Logger } from 'commandkit/logger';
+import { generateDependencyReport } from '@discordjs/voice';
 
 const handler: EventHandler<'clientReady'> = async (client) => {
   Logger.info(`Logged in as ${client.user.username}!`);
+  console.log(generateDependencyReport());
 };
 
 export default handler;
