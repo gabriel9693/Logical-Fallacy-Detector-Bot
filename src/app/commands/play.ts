c1ad7da6a04ad31e972a45d88f0b1c806321093c
gabriel9693 <bobeicagabriel908@gmail.com>
2026-08-05 15:48:55 +0300

    fix #1

--- src/app/commands/play.ts
+++ src/app/commands/play.ts
@@ -1,48 +1,49 @@
-import type { MessageCommand } from 'commandkit';
-import { joinVoiceChannel,  createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus, entersState} from '@discordjs/voice';
-import path from 'path'; 
+// import type { CommandData, MessageCommand } from 'commandkit';
+// import { joinVoiceChannel,  createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus, entersState} from '@discordjs/voice';
+// import path from 'path'; 
 
-export const message: MessageCommand = async (ctx) => {
-    const { message } = ctx;
-    if (message.author.bot) return;
 
-    if (message.content === '!play') {
-        const voiceChannel = message.member?.voice.channel; 
+// export const message: MessageCommand = async (ctx) => {
+//     const { message } = ctx;
+//     if (message.author.bot) return;
 
-        if (!voiceChannel) {
-             await message.reply('You need to join a voice channel first');
-            return;
-        }
+//     if (message.content === '!play') {
+//         const voiceChannel = message.member?.voice.channel; 
 
-        try {
-            const connection = joinVoiceChannel({
-                channelId: voiceChannel.id, 
-                guildId: voiceChannel.guild.id, 
-                adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-            });
+//         if (!voiceChannel) {
+//              await message.reply('You need to join a voice channel first');
+//             return;
+//         }
 
-            await entersState(connection, VoiceConnectionStatus.Ready, 20_000);
+//         try {
+//             const connection = joinVoiceChannel({
+//                 channelId: voiceChannel.id, 
+//                 guildId: voiceChannel.guild.id, 
+//                 adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+//             });
 
-            const player = createAudioPlayer();
-            const filePath = path.join(__dirname, '3am_spotdown.org.mp3'); 
-            const resource = createAudioResource(filePath);
+//             await entersState(connection, VoiceConnectionStatus.Ready, 20_000);
+
+//             const player = createAudioPlayer();
+//             const filePath = path.join(__dirname, '3am_spotdown.org.mp3'); 
+//             const resource = createAudioResource(filePath);
             
-            player.play(resource);
-            connection.subscribe(player);
+//             player.play(resource);
+//             connection.subscribe(player);
 
-            await message.reply("Now playing audio!");
+//             await message.reply("Now playing audio!");
 
-            player.on(AudioPlayerStatus.Idle, () => {
-                connection.destroy();
-            });
+//             player.on(AudioPlayerStatus.Idle, () => {
+//                 connection.destroy();
+//             });
 
-            player.on('error', (error) => {
-                console.error(`Audio Player Error: ${error.message}`);
-                connection.destroy();
-            });            
-        } catch (error) {
-            console.error(error);
-            await message.reply('Failed to join or play audio in the voice channel.');            
-        }
-    }
-};
+//             player.on('error', (error) => {
+//                 console.error(`Audio Player Error: ${error.message}`);
+//                 connection.destroy();
+//             });            
+//         } catch (error) {
+//             console.error(error);
+//             await message.reply('Failed to join or play audio in the voice channel.');            
+//         }
+//     }
+// };
---
+++ src/app/events/messageCreate/play.ts
@@ -0,0 +1,49 @@
+import type { EventHandler } from 'commandkit';
+import { joinVoiceChannel,  createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus, entersState} from '@discordjs/voice';
+import path from 'path'; 
+
+const handler: EventHandler<'messageCreate'> = async (message) => {
+    if (message.author.bot) return;
+
+    if (message.content === '!play') {
+        const voiceChannel = message.member?.voice.channel; 
+
+        if (!voiceChannel) {
+             await message.reply('You need to join a voice channel first');
+            return;
+        }
+
+        try {
+            const connection = joinVoiceChannel({
+                channelId: voiceChannel.id, 
+                guildId: voiceChannel.guild.id, 
+                adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+            });
+
+            await entersState(connection, VoiceConnectionStatus.Ready, 20_000);
+
+            const player = createAudioPlayer();
+            const filePath = path.join(__dirname, '3am_spotdown.org.mp3'); 
+            const resource = createAudioResource(filePath);
+            
+            player.play(resource);
+            connection.subscribe(player);
+
+            await message.reply("Now playing audio!");
+
+            player.on(AudioPlayerStatus.Idle, () => {
+                connection.destroy();
+            });
+
+            player.on('error', (error) => {
+                console.error(`Audio Player Error: ${error.message}`);
+                connection.destroy();
+            });            
+        } catch (error) {
+            console.error(error);
+            await message.reply('Failed to join or play audio in the voice channel.');            
+        }
+    }    
+};
+
+export default handler;
