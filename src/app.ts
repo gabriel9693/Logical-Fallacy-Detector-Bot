be06e23ddbb48f15dbfde830b5bfd416e7716a7d
gabriel9693 <bobeicagabriel908@gmail.com>
2026-08-05 15:45:11 +0300

    playing mp3 before test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { Client } from 'discord.js';
 
 const client = new Client({
-  intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
+  intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent', 'GuildVoiceStates'],
 });
 
 export default client;
